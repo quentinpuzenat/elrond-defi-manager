951295799a130c9210ac8bbd319be44df198e476
Quentin Puzenat <qpuzenat@gmail.com>
2021-08-23 11:34:40 +0200

    folder architecture

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+import MyNavbar from './MyNavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <MyNavbar />
+    </div>
+  );
+}
+
+export default App;
