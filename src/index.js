2c669c071ab6d5ecece05bb9bf58c45097ad692e
Quentin Puzenat <qpuzenat@gmail.com>
2021-08-23 11:33:46 +0200

    added responsive navbar

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
