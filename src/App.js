e4a08cb1e1ca187fd2a93527c4a921e873430b73
Quentin Puzenat <qpuzenat@gmail.com>
2021-08-27 10:34:57 +0200

    added root files

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {useState} from 'react';
+
+//import * as Dapp from "@elrondnetwork/dapp";
+//import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import MyNavbar from './components/MyNavbar';
+import MainDashboard from './components/MainDashboard';
+import Form from './components/Form';
+
+
+function App() {
+
+  let [myAddress, setMyAddress] = useState("");
+
+  const handleAddressCallback = (childData) => {
+        setMyAddress(childData);
+
+  
+  }
+
+  return (
+    <div className="App bg-white">
+      {/* <MyNavbar /> */}
+      <Form parentCallback={ handleAddressCallback }/>
+      <MainDashboard myAddress={ myAddress }/>
+    </div>
+  );
+}
+
+export default App;
