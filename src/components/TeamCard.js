a0406e9a2d4b291483ab8597b4a58028168b9130
Quentin Puzenat <qpuzenat@gmail.com>
2021-09-08 23:37:19 +0200

    added upgrades

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,35 +1,47 @@
-import github_photo from "../assets/images/github.svg"
+import github_photo from '../assets/images/github.svg';
 
-const TeamCard = ({name, job, picture}) => {
-    return ( 
-        <>
-        <div className=" bg-white flex flex-col gap-1 w-auto h-auto justify-items-center ring-2 ring-white rounded shadow-xl mb-4">
-                <div className="flex justify-center mb-2 ">
-                    <img src={picture} alt="default picture" className="mt-4 w-24 h-24 mx-6 ring-1 ring-white shadow-xl rounded-full"/>
-                </div>
-                <div className="flex justify-center text-gray-400 mx-6 mt-2 text-xs font-light">
-                    <p>{job}</p>
-                </div>
-                <div className="flex justify-center mx-6">
-                    <p className="m-auto text-2xl text-semibold text-gray-800 font-medium">{name}</p>
-                </div>
-                {/* on crée une border de taille voulue comme ça */}
-                <div className="flex justify-center mx-6">
-                    <p className="m-auto w-auto border-b-2 border-gray-300 text-white text-xs">aaaaaa</p>
-                </div>
-                <div className="flex justify-center mx-6">
-                    <p className="m-auto mt-2 w-64 p-2 text-sm text-center text-gray-800 font-light">French engineering student in Mathematics and Applied Statistics. Blockchain enthusiast and self-taught front-end web developer.</p>
-                </div>
-                <div className="flex justify-center mx-6 mt-1 mb-4">
-                    <a href="https://github.com/quentinpuzenat" target="_blank"><img src={github_photo} alt="github picture" className="w-5 h-5"/></a>
-                </div>
+const TeamCard = ({ name, job, picture, experience }) => {
+   return (
+      <>
+         <div className=" bg-white flex flex-col gap-1 w-auto h-auto justify-items-center ring-2 ring-white rounded shadow-xl mb-8">
+            <div className="flex justify-center mb-2 ">
+               <img
+                  src={picture}
+                  alt="default picture"
+                  className="mt-4 w-24 h-24 mx-6 ring-1 ring-white shadow-xl rounded-full"
+               />
+            </div>
+            <div className="flex justify-center text-gray-400 mx-6 mt-2 text-xs font-light">
+               <p>{job}</p>
+            </div>
+            <div className="flex justify-center mx-6">
+               <p className="m-auto text-2xl text-semibold text-gray-800 font-medium">
+                  {name}
+               </p>
+            </div>
+            {/* on crée une border de taille voulue comme ça */}
+            <div className="flex justify-center mx-6">
+               <p className="m-auto w-auto border-b-2 border-gray-300 text-white text-xs">
+                  aaaaaa
+               </p>
+            </div>
+            <div className="flex justify-center mx-6">
+               <p className="m-auto mt-2 w-64 p-2 text-sm text-center text-gray-800 font-light">
+                  {experience}
+               </p>
+            </div>
+            <div className="flex justify-center mx-6 mt-1 mb-4">
+               <a href="https://github.com/quentinpuzenat" target="_blank">
+                  <img
+                     src={github_photo}
+                     alt="github picture"
+                     className="w-5 h-5"
+                  />
+               </a>
+            </div>
+         </div>
+      </>
+   );
+};
 
-
-                
-            
-        </div>
-        </>
-     );
-}
- 
 export default TeamCard;
--- src/pages/Team.js
+++ src/pages/Team.js
@@ -1,66 +1,36 @@
 import TeamCard from '../components/TeamCard';
 import defaultPicture from '../assets/images/tanjiro_pp.png';
+import { useContext } from 'react';
+import { AddressContext } from '../context';
 
 const Team = () => {
+   const { input } = useContext(AddressContext);
+   console.log(`Value input:${input}`);
+
    return (
       <>
          <div className="h-full bg-gray-50 bg-opacity-75 pt-20">
+            <div>{input !== undefined ? input : 'no value provided'}</div>
             <div className="flex justify-center">
                <h1 className="mt-4 text-gray-900 font-semibold text-2xl">
                   Our Team
                </h1>
             </div>
-            <div className="w-full h-full mt-4 flex flex-wrap gap-8 justify-center">
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
-               <TeamCard
-                  name="Quentin Puzenat"
-                  job="FRONT-END WEB DEVELOPER"
-                  picture={defaultPicture}
-               />
+            <div className="flex justify-center ">
+               <div className="w-full h-full mt-4 flex flex-wrap gap-8 justify-center">
+                  <TeamCard
+                     name="Quentin Puzenat"
+                     job="FRONT-END WEB DEVELOPER"
+                     picture={defaultPicture}
+                     experience="French engineering student in Mathematics and Applied Statistics. Blockchain enthusiast and self-taught front-end web developer."
+                  />
+                  <TeamCard
+                     name="Loïc Thiriet"
+                     job="FRONT-END WEB DEVELOPER"
+                     picture={defaultPicture}
+                     experience="French engineering student in data science, passionated by Elrond blockchain."
+                  />
+               </div>
             </div>
          </div>
       </>
