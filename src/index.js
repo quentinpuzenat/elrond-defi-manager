334889f2f0bc55e4a3689e799d7aec9f3a5743c1
Quentin Puzenat <qpuzenat@gmail.com>
2021-09-11 18:30:30 +0200

    issue with imports, needed ContextProvider

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import DashboardPage from './pages/DashboardPage';
 import Layout from './components/Layout';
 import Error from './components/Error404';
 import AddressProvider from './context';
+import { ContextProvider } from './context/contextProvider';
 import routes, { routeNames } from './routes';
 
 import reportWebVitals from './reportWebVitals';
@@ -42,75 +43,79 @@ console.log(routes);
 ReactDOM.render(
    <React.StrictMode>
       <Router>
-         <Dapp.Context
-            config={{
-               network, // provide connection information
-               walletConnectBridge, // the server used to relay data between the Dapp and the Wallet
-               walletConnectDeepLink, // link used to open the Maiar app with the connection details
-            }}
-         >
-            <ApolloProvider client={MaiarExchangeClient}>
-               <AddressProvider>
-                  <Layout>
-                     <Switch>
-                        {/* <Route path="/" exact>
-                           <Home />
-                        </Route>
-                        <Route path="/team" exact>
-                           <Team />
-                        </Route>
-                        <Route path="/:erdAddress">
-                           <DashboardPage />
-                        </Route> */}
-                        <Route
-                           path={routeNames.unlock}
-                           component={() => (
-                              <Dapp.Pages.Unlock
-                                 callbackRoute={routeNames.dashboard}
-                                 lead="Please select your login method:"
-                                 ledgerRoute={routeNames.ledger}
-                                 walletConnectRoute={routeNames.walletconnect}
-                              />
-                           )}
-                           exact={true}
-                        />
-                        <Route
-                           path={routeNames.ledger}
-                           component={() => (
-                              <Dapp.Pages.Ledger
-                                 callbackRoute={routeNames.dashboard}
-                              />
-                           )}
-                           exact={true}
-                        />
-                        <Route
-                           path={routeNames.walletconnect}
-                           component={() => (
-                              <Dapp.Pages.WalletConnect
-                                 callbackRoute={routeNames.dashboard}
-                                 logoutRoute={routeNames.home}
-                                 title="Maiar Login"
-                                 lead="Scan the QR code using Maiar"
-                              />
-                           )}
-                           exact={true}
-                        />
-                        {routes.map((route, i) => (
+         <ContextProvider>
+            <Dapp.Context
+               config={{
+                  network, // provide connection information
+                  walletConnectBridge, // the server used to relay data between the Dapp and the Wallet
+                  walletConnectDeepLink, // link used to open the Maiar app with the connection details
+               }}
+            >
+               <ApolloProvider client={MaiarExchangeClient}>
+                  <AddressProvider>
+                     <Layout>
+                        <Switch>
+                           {/* <Route path="/" exact>
+                              <Home />
+                           </Route>
+                           <Route path="/team" exact>
+                              <Team />
+                           </Route>
+                           <Route path="/:erdAddress">
+                              <DashboardPage />
+                           </Route> */}
                            <Route
-                              path={route.path}
-                              key={route.path + i}
-                              component={route.component}
+                              path={routeNames.unlock}
+                              component={() => (
+                                 <Dapp.Pages.Unlock
+                                    callbackRoute={routeNames.dashboard}
+                                    lead="Please select your login method:"
+                                    ledgerRoute={routeNames.ledger}
+                                    walletConnectRoute={
+                                       routeNames.walletconnect
+                                    }
+                                 />
+                              )}
                               exact={true}
                            />
-                        ))}
-                        <Route>
-                           <Error />
-                        </Route>
-                     </Switch>
-                  </Layout>
-               </AddressProvider>
-            </ApolloProvider>
-         </Dapp.Context>
+                           <Route
+                              path={routeNames.ledger}
+                              component={() => (
+                                 <Dapp.Pages.Ledger
+                                    callbackRoute={routeNames.dashboard}
+                                 />
+                              )}
+                              exact={true}
+                           />
+                           <Route
+                              path={routeNames.walletconnect}
+                              component={() => (
+                                 <Dapp.Pages.WalletConnect
+                                    callbackRoute={routeNames.dashboard}
+                                    logoutRoute={routeNames.home}
+                                    title="Maiar Login"
+                                    lead="Scan the QR code using Maiar"
+                                 />
+                              )}
+                              exact={true}
+                           />
+                           {routes.map((route, i) => (
+                              <Route
+                                 path={route.path}
+                                 key={route.path + i}
+                                 component={route.component}
+                                 exact={true}
+                              />
+                           ))}
+                           <Route>
+                              <Error />
+                           </Route>
+                        </Switch>
+                     </Layout>
+                  </AddressProvider>
+               </ApolloProvider>
+            </Dapp.Context>
+         </ContextProvider>
       </Router>
    </React.StrictMode>,
    document.getElementById('root')
